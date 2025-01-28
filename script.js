93467ba062a9184f5c57bd92929ef63ac210ebd4
vinimaffra03 <viniciushmdem15@gmail.com>
2025-01-28 07:25:15 -0300

    integração js

---
+++ script.js
@@ -0,0 +1,56 @@
+const item = document.getElementById('input-item');
+const botaoSalvarItem = document.getElementById('adicionar-tem');
+const listaDeCompras = document.getElementById('lista-de-compras');
+let contador = 0;
+
+botaoSalvarItem.addEventListener('click', adicionarItem);
+
+function adicionarItem(evento) {
+    evento.preventDefault();
+    
+    const itemDaLista = document.createElement('li');
+    const containerItemLista = document.createElement('div');
+    containerItemLista.classList.add('lista-item-container');
+
+    const containerNomeDoItem = document.createElement('div');
+
+    const containerCheckbox = document.createElement('div');
+    containerCheckbox.classList,add('container-checkbox');
+
+    const checkboxInput = document.createElement('input');
+    checkboxInput.type = 'checkbox';
+    checkboxInput.classList.add('input-checkbox');
+    checkboxInput.id = 'checkbox-' + contador++;
+
+    const checkboxLabel = document.createElement('label');
+    checkboxLabel.setAttribute('for', checkboxInput.id);
+
+    const checkboxCustomizado = document.createElement('div');
+    checkboxCustomizado.classList.add('checkbox-customizado');
+
+    checkboxLabel.appendChild(checkboxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    containerCheckbox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckbox);
+
+    const nomeDoItem = document.createElement('div');
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement('div');
+    const botaoRemover = document.createElement('button');
+    botaoRemover.classList.add('item-lista-button');
+
+    const imagemRemover = document.createElement('img');
+    imagemRemover.src = 'img/delete.svg';
+    imagemRemover.alt = 'remover';
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
+}
